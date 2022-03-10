0d18ee4f9e8f2a0b84bd7805affee8100188d05d
Felipe Fraxe <felipe_fraxe@hotmail.com>
2022-03-10 15:21:18 -0400

    Ajustes do lint nas chamadas de API

--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,32 @@
-const BASE_URL = 'https://api.mercadolibre.com/sites/MLB';
-
 /*
   Função pra realizar as chamadas de api genéricas.
   Parâmetros: URL principal da api, endpoint específica da api, callback para o tratamento dos dados
 */
-function customFetch(BASE_URL, ENDPOINT, dataCallback) {
+
+function customFetch(ENDPOINT, dataCallback) {
+  const BASE_URL = 'https://api.mercadolibre.com/sites/MLB';
   return fetch(BASE_URL + ENDPOINT)
-  .then((response) => response.json())
-  .then(dataCallback)
-  .catch((error) => console.log('Error: ', error));
+    .then((response) => response.json())
+    .then(dataCallback)
+    .catch((error) => console.log('Error: ', error));
 }
 
-// Callback para o tratamento dos dados da getCategory
+//  Callback para o tratamento dos dados da getCategory
 function categoriesCallBack(data) {
   return data;
 }
 
 export async function getCategories() {
   const CATEGORIES_ENDPOINT = '/categories';
-  return await customFetch(BASE_URL, CATEGORIES_ENDPOINT, categoriesCallBack);
+  return customFetch(CATEGORIES_ENDPOINT, categoriesCallBack);
 }
 
-//Callback para o tratamento dos dados da getProductsFromCategoryAndQuery
+//  Callback para o tratamento dos dados da getProductsFromCategoryAndQuery
 function productsFromCategoriesCallback(data) {
   return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const PRODUCTS_FROM_CATEGORIES_ENDPOINT = `/search?category=${categoryId}&q=${query}`;
-  return await customFetch(BASE_URL, PRODUCTS_FROM_CATEGORIES_ENDPOINT, productsFromCategoriesCallback);
+  return customFetch(PRODUCTS_FROM_CATEGORIES_ENDPOINT, productsFromCategoriesCallback);
 }
