f459994ed93d83ae39e6b614d0f7e9c684a4d209
Guilherme Lara <guilaraa@gmail.com>
2022-03-10 17:44:11 -0300

    Adicionando CSS a HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import '../css/Home.css';
+
+// Cria O input para busca e o texto solicitado
 
 export default class HomePage extends Component {
   render() {
     return (
       <>
-        <input
-          className="input-search"
-          name="input-search"
-          type="text"
-          placeholder="Buscar..."
-        />
-        <h1 data-testid="home-initial-message">
+        <div className="input-position">
+          <input
+            className="input-search"
+            name="input-search"
+            type="text"
+            placeholder="Buscar..."
+          />
+        </div>
+        <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
+        </h2>
       </>
     );
   }
