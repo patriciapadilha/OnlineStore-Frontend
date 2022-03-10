31ab125fa5d45c2033bdf9e0c70f34bac28b3356
Felipe Fraxe <felipe_fraxe@hotmail.com>
2022-03-10 15:00:44 -0400

    Requisito 1. Chamadas para a API do mercado livre

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,32 @@
-export async function getCategories() {
-  // Implemente aqui
+const BASE_URL = 'https://api.mercadolibre.com/sites/MLB';
+
+/*
+  Função pra realizar as chamadas de api genéricas.
+  Parâmetros: URL principal da api, endpoint específica da api, callback para o tratamento dos dados
+*/
+function customFetch(BASE_URL, ENDPOINT, dataCallback) {
+  return fetch(BASE_URL + ENDPOINT)
+  .then((response) => response.json())
+  .then(dataCallback)
+  .catch((error) => console.log('Error: ', error));
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+// Callback para o tratamento dos dados da getCategory
+function categoriesCallBack(data) {
+  return data;
+}
+
+export async function getCategories() {
+  const CATEGORIES_ENDPOINT = '/categories';
+  return await customFetch(BASE_URL, CATEGORIES_ENDPOINT, categoriesCallBack);
+}
+
+//Callback para o tratamento dos dados da getProductsFromCategoryAndQuery
+function productsFromCategoriesCallback(data) {
+  return data;
+}
+
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const PRODUCTS_FROM_CATEGORIES_ENDPOINT = `/search?category=${categoryId}&q=${query}`;
+  return await customFetch(BASE_URL, PRODUCTS_FROM_CATEGORIES_ENDPOINT, productsFromCategoriesCallback);
 }
