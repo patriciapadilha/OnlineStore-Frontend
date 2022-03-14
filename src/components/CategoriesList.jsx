fff2eed21f49fb5f136322c03bb4146d6d5b9b7d
Debora Schuch da Rosa <starambients@gmail.com>
2022-03-14 20:50:42 +0100

    [add] requisito 6 - mostra produtos por categoria selecionada

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import * as api from '../services/api';
+import Card from './Card';
 
 export default class CategoriesList extends Component {
   constructor() {
     super();
     this.state = {
       categories: [],
+      isChecked: false,
+      productsCard: [],
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.fetchGetProductsFromCategoryAndQuery = this
+      .fetchGetProductsFromCategoryAndQuery.bind(this);
   }
   // Não permite que a inicialização da pagína ocorra sem a chamada da API
 
@@ -18,8 +24,26 @@ export default class CategoriesList extends Component {
     });
   }
 
+  handleChange(event) {
+    this.setState((prevState) => ({
+      isChecked: !prevState.isChecked }));
+    const { target } = event;
+    const { checked, id } = target;
+    if (checked) {
+      this.fetchGetProductsFromCategoryAndQuery(id);
+    }
+  }
+
+  async fetchGetProductsFromCategoryAndQuery(_id) {
+    const response = await api.getProductsFromCategoryAndQuery(_id, '');
+    console.log(response);
+    this.setState({
+      productsCard: response,
+    });
+  }
+
   render() {
-    const { categories } = this.state;
+    const { categories, isChecked, productsCard } = this.state;
     /* Map percorre todas as categorias retornadas pela API e entrega uma UL com botões para seleção */
     const categoriesList = (
       categories
@@ -31,18 +55,37 @@ export default class CategoriesList extends Component {
                 type="radio"
                 name="inputCategories"
                 value={ name }
+                checked={ isChecked }
+                onChange={ this.handleChange }
               />
               {name}
             </label>
           </li>))
     );
+    const products = (
+      productsCard.map(({ title, thumbnail, price }, key) => (
+        <Card
+          key={ key }
+          title={ title }
+          thumbSrc={ thumbnail }
+          price={ price }
+        />
+      ))
+    );
+
     return (
-      <aside className="container-categories">
-        <h3>Categorias</h3>
-        <ul>
-          { categoriesList }
-        </ul>
-      </aside>
+      <>
+        <aside className="container-categories">
+          <h3>Categorias</h3>
+          <ul>
+            { categoriesList }
+          </ul>
+        </aside>
+        <div className="allProductsByCategory">
+          { products }
+        </div>
+      </>
+
     );
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -22,27 +22,27 @@ export async function getCategories() {
 }
 
 //  Callback para o tratamento dos dados da getProductsFromCategoryAndQuery
-function productsFromCategoriesCallback(data) {
-  return data;
-}
-
-export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const PRODUCTS_FROM_CATEGORIES_ENDPOINT = `/search?category=${categoryId}&q=${query}`;
-  return customFetch(PRODUCTS_FROM_CATEGORIES_ENDPOINT, productsFromCategoriesCallback);
-}
-
-//  Callback para o tratamento dos dados da getProductsByQuery
-function getProductsByQueryCallback({ results }) {
-  return results.map(({ title, price, thumbnail }) => (
+function productsFromCategoriesAndQueryCallback({ results }) {
+  return results.map(({ title, price, thumbnail, id }) => (
     {
       title,
       price,
       thumbnail,
+      id,
     }
   ));
 }
 
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const PRODUCTS_FROM_CATEGORIES_ENDPOINT = `/search?category=${categoryId}&q=${query}`;
+  return customFetch(
+    PRODUCTS_FROM_CATEGORIES_ENDPOINT, productsFromCategoriesAndQueryCallback,
+  );
+}
+
 export async function getProductsByQuery(query) {
   const PRODUCTS_FROM_QUERY_ENDPOINT = `/search?q=${query}`;
-  return customFetch(PRODUCTS_FROM_QUERY_ENDPOINT, getProductsByQueryCallback);
+  return customFetch(
+    PRODUCTS_FROM_QUERY_ENDPOINT, productsFromCategoriesAndQueryCallback,
+  );
 }
