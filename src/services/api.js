7f6758d5982ff3d7abad3a6e000141639e4cf72b
Felipe Fraxe <felipe_fraxe@hotmail.com>
2022-03-14 20:42:01 -0400

    [fix] - requisito 1 - padroniza as callbacks em uma só

--- src/services/api.js
+++ src/services/api.js
@@ -1,41 +1,27 @@
 /*
   Função pra realizar as chamadas de api genéricas.
-  Parâmetros: URL principal da api, endpoint específica da api, callback para o tratamento dos dados
+  Parâmetros: URL principal da api, endpoint específica da api
 */
 
-function customFetch(ENDPOINT, dataCallback) {
+function customFetch(ENDPOINT) {
   const BASE_URL = 'https://api.mercadolibre.com/sites/MLB';
   return fetch(BASE_URL + ENDPOINT)
     .then((response) => response.json())
-    .then(dataCallback)
+    .then((data) => data)
     .catch((error) => console.log('Error: ', error));
-}
-
-//  Callback para o tratamento dos dados da getCategory
-function categoriesCallBack(data) {
-  return data;
 }
 
 export async function getCategories() {
   const CATEGORIES_ENDPOINT = '/categories';
-  return customFetch(CATEGORIES_ENDPOINT, categoriesCallBack);
-}
-
-//  Callback para o tratamento dos dados da getProductsFromCategoryAndQuery
-function productsFromCategoriesAndQueryCallback(data) {
-  return data;
+  return customFetch(CATEGORIES_ENDPOINT);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const PRODUCTS_FROM_CATEGORIES_ENDPOINT = `/search?category=${categoryId}&q=${query}`;
-  return customFetch(
-    PRODUCTS_FROM_CATEGORIES_ENDPOINT, productsFromCategoriesAndQueryCallback,
-  );
+  return customFetch(PRODUCTS_FROM_CATEGORIES_ENDPOINT);
 }
 
 export async function getProductsByQuery(query) {
   const PRODUCTS_FROM_QUERY_ENDPOINT = `/search?q=${query}`;
-  return customFetch(
-    PRODUCTS_FROM_QUERY_ENDPOINT, productsFromCategoriesAndQueryCallback,
-  );
+  return customFetch(PRODUCTS_FROM_QUERY_ENDPOINT);
 }
