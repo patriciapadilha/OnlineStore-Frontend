328ec44902a82b787fc562c04443ea3bea83ac2c
Lindonjonson Filho <lindojhonsonf@gmail.com>
2022-03-15 19:39:17 -0300

    [add] - req 8 parcial

--- src/Pages/ShoppingCart/ShowProduct.jsx
+++ src/Pages/ShoppingCart/ShowProduct.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 
 export default class ShowProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productsInCart: [],
+    };
+  }
+
+  componentDidMount() {
+    this.showProductsInCart();
+  }
+
+  showProductsInCart = () => {
+    const storage = JSON.parse(localStorage.getItem('cartItems'));
+    this.setState({ productsInCart: storage });
+  };
+
   render() {
+    const { productsInCart } = this.state;
     return (
-      <div data-testid="shopping-cart-empty-message">
-        <p>Seu carrinho está vazio</p>
+      <div>
+        {productsInCart ? (
+          productsInCart.map((items) => (
+            <div key={ items.id }>
+              <p data-testid="shopping-cart-product-name">{ items.title }</p>
+              <p data-testid="shopping-cart-product-quantity">
+                <label
+                  htmlFor="quantity"
+                  datatestid="shopping-cart-product-quantity"
+                >
+                  Quantidade:
+                  <input
+                    type="number"
+                    name="quantity"
+                    className="quantity"
+                    defaultValue={ 1 }
+                  />
+                </label>
+              </p>
+            </div>
+          ))
+        ) : (
+          <div data-testid="shopping-cart-empty-message">
+            <p>Seu carrinho está vazio</p>
+          </div>
+        )}
+        );
       </div>
     );
   }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,17 +6,33 @@ export default class Card extends Component {
   render() {
     const { title, thumbSrc, price, id } = this.props;
     return (
-      <Link
-        data-testid="product-detail-link"
-        to={ `/product-detail/${id}` }
-      >
-        <div data-testid="product">
-          <p>{ title }</p>
-          <img src={ thumbSrc } alt={ `Imagem de ${title}` } />
-          <p>{`Valor R$ ${price.toFixed(2)}`}</p>
-          Ver detalhes:
-        </div>
-      </Link>
+      <div>
+        <Link data-testid="product-detail-link" to={ `/product-detail/${id}` }>
+          <div data-testid="product">
+            <p>{title}</p>
+            <img src={ thumbSrc } alt={ `Imagem de ${title}` } />
+            <p>{`Valor R$ ${price}`}</p>
+            Ver detalhes:
+          </div>
+        </Link>
+        <button
+          data-testid="product-add-to-cart"
+          type="button"
+          name="addToCart"
+          onClick={ this.addToCart }
+        >
+          Adicionar ao carrinho
+        </button>
+        <label htmlFor="quantity" datatestid="shopping-cart-product-quantity">
+          Quantidade:
+          <input
+            type="number"
+            name="quantity"
+            className="quantity"
+            defaultValue={ 1 }
+          />
+        </label>
+      </div>
     );
   }
 }
